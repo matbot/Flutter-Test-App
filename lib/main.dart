a96b05f15e50d7f466aee1d77683bd0cdf80fe43
matbotW <mathewmcdade@gmail.com>
2019-12-25 13:51:54 -0800

    adding a column

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,35 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Flutter Testing App'),
+    var questions = [
+      "What's your name?",
+      "What's your age",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Testing App'),
+        ),
+        body: Column(
+          children: [
+            Text('Question1'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: null,
+            ),
+            Text('Question2'),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: null,
+            ),
+            Text('Question3'),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
-      body: Text('Testing body text'),
-    ),
     );
   }
 }
